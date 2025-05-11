9d6f0f5007d8fbe44ac5c48679d6e32290098d30
omarkamal62 <okamal34@gmail.com>
2025-05-11 04:40:10 +0300

    adding mongo to the app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseConfig } from './config/database.config';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(DatabaseConfig.uri)],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/database.config.ts
@@ -0,0 +1,3 @@
+export class DatabaseConfig {
+  static uri = 'mongodb://localhost:27017/nest-user-auth';
+}
